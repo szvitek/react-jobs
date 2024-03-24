f567afd3af6dd9b08ee089ae8dacdc36f3cfa243
szvitek <gy.szvitek@gmail.com>
2024-03-24 13:09:43 +0000

    feat(Tailwind): install tailwind css

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif']
+      },
+      gridTemplateColumns: {
+        '70/30': '70% 28%'
+      }
+    },
+  },
+  plugins: [],
+} satisfies Config;
+
