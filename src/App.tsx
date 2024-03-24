a731eeafa0469ac5962572b8739f6b5a34e311da
szvitek <gy.szvitek@gmail.com>
2024-03-24 19:49:57 +0000

    feat(Router): fix Links, nav active class, jobs page, not found page created

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,15 @@ import {
 } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
+import JobsPage from './pages/JobsPage';
+import NotFound from './pages/NotFound';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage />} />
+      <Route path="*" element={<NotFound />} />
     </Route>
   )
 );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,7 +8,7 @@ const HomePage = () => {
     <>
       <Hero />
       <Cards />
-      <JobListings />
+      <JobListings isHome={true} />
       <ViewAllJobs />
     </>
   );
